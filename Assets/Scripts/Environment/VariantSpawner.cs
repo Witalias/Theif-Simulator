e9180a8f6d9a060cfdc27c3a96f2a3647af4e8ec
Vitaliy Dmitriev <dmitriz_vitaliy@mail.ru>
2023-11-23 13:46:41 +0500

    Fixes

--- Assets/Scripts/Environment/VariantSpawner.cs
+++ Assets/Scripts/Environment/VariantSpawner.cs
@@ -4,10 +4,10 @@ public class VariantSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] variants;
 
-    private void Awake()
-    {
-        var randomVariant = variants[Random.Range(0, variants.Length)];
-        Instantiate(randomVariant, transform.position, transform.rotation, transform.parent);
-        Destroy(gameObject);
-    }
+    //private void Awake()
+    //{
+    //    var randomVariant = variants[Random.Range(0, variants.Length)];
+    //    Instantiate(randomVariant, transform.position, transform.rotation, transform.parent);
+    //    Destroy(gameObject);
+    //}
 }
